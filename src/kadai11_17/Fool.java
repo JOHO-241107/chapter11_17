925748a32b1dfe7d5cd52c9faa568470c2b99c03
HoriMasataka <241107@std.hi-joho.ac.jp>
2025-06-18 09:58:25 +0900

    コード11-17

--- src/kadai11_17/Fool.java
+++ src/kadai11_17/Fool.java
@@ -14,8 +14,8 @@ public class Fool extends Character implements Human {
         System.out.println(this.name + "は誰かの声を聞いた気がした");
     }
     public void run(){System.out.println(this.name + "は大笑いしながら逃げ出した");}
-    public Fool(String n, int h){
-        this.name = n;
-        this.hp = h;
+    public Fool(String inputName, int hitpoint){
+        this.name = inputName;
+        this.hp = hitpoint;
     }
 }
